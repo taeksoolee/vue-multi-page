9daeb26775e73127fc44f3ac47b3183699f5945f
taeksoolee <leets1490@gmail.com>
2025-01-05 00:15:02 +0900

    테스트 서버 구성

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # zeb platform
 
-- application : web / admin / cms
+## stack
+
+## web
+> ssg + spa
+- static 배포
+- `/index.html`, `/about.html` 등 으로 접근
+
+## admin
+> spa
+- spa 배포 (유효한 파일 외 `index.html` 파일 제공)
+
+## cms
+> spa
+- spa 배포 (유효한 파일 외 `index.html` 파일 제공)
--- server/index.js
+++ server/index.js
@@ -1,13 +1,59 @@
-import e from "express";
+import koa from 'koa';
+import serve from "koa-static";
+import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url"; 
 
-const app = e();
-
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
-app.use(e.static(path.resolve(__dirname, '..', 'web', 'dist')));
-
-app.listen(4000, () => {
-  console.log('Server is running on http://localhost:4000');
+const web = new koa();
+const webStaticPath = path.resolve(__dirname, '..', 'web', 'dist');
+web.use(serve(webStaticPath));
+web.use(async (ctx) => {
+  const filePath = path.join(webStaticPath, ctx.path);
+  if (fs.existsSync(filePath) && fs.lstatSync(filePath).isFile()) {
+    ctx.type = path.extname(filePath);
+    ctx.body = fs.createReadStream(filePath);
+  } else {
+    ctx.type = 'text/html';
+    ctx.body = fs.createReadStream(path.join(webStaticPath, 'index.html'));
+  }
 });
+web.listen(3001, () => {
+  console.log('Web Server is running on http://localhost:3001');
+});
+
+const admin = new koa();
+const adminStaticPath = path.resolve(__dirname, '..', 'admin', 'dist');
+admin.use(serve(adminStaticPath));
+admin.use(async (ctx) => {
+  const filePath = path.join(adminStaticPath, ctx.path);
+  if (fs.existsSync(filePath) && fs.lstatSync(filePath).isFile()) {
+    ctx.type = path.extname(filePath);
+    ctx.body = fs.createReadStream(filePath);
+  } else {
+    ctx.type = 'text/html';
+    ctx.body = fs.createReadStream(path.join(adminStaticPath, 'index.html'));
+  }
+});
+admin.listen(3002, () => {
+  console.log('Web Server is running on http://localhost:3002');
+});
+
+const cms = new koa();
+const cmsStaticPath = path.resolve(__dirname, '..', 'cms', 'dist');
+cms.use(serve(cmsStaticPath));
+cms.use(async (ctx) => {
+  const filePath = path.join(cmsStaticPath, ctx.path);
+  if (fs.existsSync(filePath) && fs.lstatSync(filePath).isFile()) {
+    ctx.type = path.extname(filePath);
+    ctx.body = fs.createReadStream(filePath);
+  } else {
+    ctx.type = 'text/html';
+    ctx.body = fs.createReadStream(path.join(cmsStaticPath, 'index.html'));
+  }
+});
+cms.listen(3003, () => {
+  console.log('Web Server is running on http://localhost:3003');
+});
+
